4990a208ca7b0f1eea37842a7db96f07c791aaa4
Zifkage <zifkage@gmail.com>
2019-02-15 21:07:01 +0100

    Add route to Worshop component

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Home from './components/home';
 import Login from './components/login';
 import Register from './components/register';
 import Profile from './components/profile';
+import Workshop from './components/workshop';
 import { Route } from 'react-router-dom';
 
 class App extends Component {
@@ -16,6 +17,7 @@ class App extends Component {
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
           <Route path="/profile" component={Profile} />
+          <Route path="/workshop" component={Workshop} />
         </div>
       </div>
     );
