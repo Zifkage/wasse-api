39ae0f630d98cc76379b9ac86aba38e0bb28799b
Zifkage <zifkage@gmail.com>
2019-02-15 20:57:20 +0100

    Add route to Register component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from './components/navbar';
 import Home from './components/home';
 import Login from './components/login';
+import Register from './components/register';
 import { Route } from 'react-router-dom';
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
         <div className="container">
           <Route exact path="/" component={Home} />
           <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
         </div>
       </div>
     );
