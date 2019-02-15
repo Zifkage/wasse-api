b950f68f7e85f7d17997479c48e50f4bfe637a74
Zifkage <zifkage@gmail.com>
2019-02-15 20:40:46 +0100

    Add routes to home and login components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import Navbar from './components/navbar';
+import Home from './components/home';
+import Login from './components/login';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
-        <div className="container">hello</div>
+        <div className="container">
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+        </div>
       </div>
     );
   }
---
+++ client/src/components/login.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function login(prop) {
+  return <div>login</div>;
+}
+
+export default login;
