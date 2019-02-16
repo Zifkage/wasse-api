46ad2bd3a561b2f2c083d2e329180b4b01829f22
Zifkage <zifkage@gmail.com>
2019-02-16 20:57:01 +0100

    Handler user logout

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,66 +1,76 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
-const navbar = () => {
-  let currentUser = localStorage.getItem('currentUser');
-  if (currentUser) currentUser = JSON.parse(currentUser);
-  return (
-    <nav>
-      <div className="nav-wrapper blue">
-        <NavLink exact to="/" className="brand-logo center">
-          Social-Aca
-        </NavLink>
-        <ul className="left hide-on-med-and-down">
-          <li>
-            <NavLink exact activeClassName="grey" to="/">
-              Accueil
-            </NavLink>
-          </li>
-          <li>
-            <NavLink activeClassName="grey" to="/workshop">
-              TD
-            </NavLink>
-          </li>
-          {currentUser && (
+class navbar extends Component {
+  onLogout = () => {
+    localStorage.removeItem('currentUser');
+    this.props.history.push('/');
+  };
+  render() {
+    let currentUser = localStorage.getItem('currentUser');
+    if (currentUser) currentUser = JSON.parse(currentUser);
+    return (
+      <nav>
+        <div className="nav-wrapper blue">
+          <NavLink exact to="/" className="brand-logo center">
+            Social-Aca
+          </NavLink>
+          <ul className="left hide-on-med-and-down">
             <li>
-              <NavLink activeClassName="grey" to="/profile">
-                Profile-({currentUser.email})
+              <NavLink exact activeClassName="grey" to="/">
+                Accueil
               </NavLink>
             </li>
-          )}
-          {!currentUser && (
             <li>
-              <NavLink
-                activeClassName="grey"
-                to="/login"
-                className="waves-effect orange  waves-light btn"
-              >
-                se connecter
+              <NavLink activeClassName="grey" to="/workshop">
+                TD
               </NavLink>
             </li>
-          )}
-          {!currentUser && (
-            <li>
-              <NavLink
-                activeClassName="grey"
-                to="/register"
-                className="waves-effect green  waves-light btn"
-              >
-                s'enregistrer
-              </NavLink>
-            </li>
-          )}
-          {currentUser && (
-            <li>
-              <button className="waves-effect red  waves-light btn">
-                se déconnecter
-              </button>
-            </li>
-          )}
-        </ul>
-      </div>
-    </nav>
-  );
-};
+            {currentUser && (
+              <li>
+                <NavLink activeClassName="grey" to="/profile">
+                  Profile-({currentUser.email})
+                </NavLink>
+              </li>
+            )}
+            {!currentUser && (
+              <li>
+                <NavLink
+                  activeClassName="grey"
+                  to="/login"
+                  className="waves-effect orange  waves-light btn"
+                >
+                  se connecter
+                </NavLink>
+              </li>
+            )}
+            {!currentUser && (
+              <li>
+                <NavLink
+                  activeClassName="grey"
+                  to="/register"
+                  className="waves-effect green  waves-light btn"
+                >
+                  s'enregistrer
+                </NavLink>
+              </li>
+            )}
+            {currentUser && (
+              <li>
+                <button
+                  onClick={(e) => this.onLogout()}
+                  className="waves-effect red  waves-light btn"
+                >
+                  se déconnecter
+                </button>
+              </li>
+            )}
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
 
-export default navbar;
+export default withRouter(navbar);
