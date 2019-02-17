de219567243438374a44aa8079ebc77a710753a7
Zifkage <zifkage@gmail.com>
2019-02-17 12:04:01 +0100

    Fixe the navbar at the top

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -11,64 +11,66 @@ class navbar extends Component {
     let currentUser = localStorage.getItem('currentUser');
     if (currentUser) currentUser = JSON.parse(currentUser);
     return (
-      <nav>
-        <div className="nav-wrapper blue">
-          <NavLink exact to="/" className="brand-logo center">
-            Social-Aca
-          </NavLink>
-          <ul className="left hide-on-med-and-down">
-            <li>
-              <NavLink exact activeClassName="grey" to="/">
-                Accueil
-              </NavLink>
-            </li>
-            <li>
-              <NavLink activeClassName="grey" to="/workshop">
-                TD
-              </NavLink>
-            </li>
-            {currentUser && (
+      <div className="navbar-fixed">
+        <nav>
+          <div className="nav-wrapper blue">
+            <NavLink exact to="/" className="brand-logo center">
+              Social-Aca
+            </NavLink>
+            <ul className="left hide-on-med-and-down">
               <li>
-                <NavLink activeClassName="grey" to="/profile">
-                  Profile-({currentUser.email})
+                <NavLink exact activeClassName="grey" to="/">
+                  Accueil
                 </NavLink>
               </li>
-            )}
-            {!currentUser && (
               <li>
-                <NavLink
-                  activeClassName="grey"
-                  to="/login"
-                  className="waves-effect orange  waves-light btn"
-                >
-                  se connecter
+                <NavLink activeClassName="grey" to="/workshop">
+                  TD
                 </NavLink>
               </li>
-            )}
-            {!currentUser && (
-              <li>
-                <NavLink
-                  activeClassName="grey"
-                  to="/register"
-                  className="waves-effect green  waves-light btn"
-                >
-                  s'enregistrer
-                </NavLink>
-              </li>
-            )}
-            {currentUser && (
-              <li>
-                <button
-                  onClick={(e) => this.onLogout()}
-                  className="waves-effect red  waves-light btn"
-                >
-                  se déconnecter
-                </button>
-              </li>
-            )}
-          </ul>
-        </div>
-      </nav>
+              {currentUser && (
+                <li>
+                  <NavLink activeClassName="grey" to="/profile">
+                    Profile-({currentUser.email})
+                  </NavLink>
+                </li>
+              )}
+              {!currentUser && (
+                <li>
+                  <NavLink
+                    activeClassName="grey"
+                    to="/login"
+                    className="waves-effect orange  waves-light btn"
+                  >
+                    se connecter
+                  </NavLink>
+                </li>
+              )}
+              {!currentUser && (
+                <li>
+                  <NavLink
+                    activeClassName="grey"
+                    to="/register"
+                    className="waves-effect green  waves-light btn"
+                  >
+                    s'enregistrer
+                  </NavLink>
+                </li>
+              )}
+              {currentUser && (
+                <li>
+                  <button
+                    onClick={(e) => this.onLogout()}
+                    className="waves-effect red  waves-light btn"
+                  >
+                    se déconnecter
+                  </button>
+                </li>
+              )}
+            </ul>
+          </div>
+        </nav>
+      </div>
     );
   }
 }
