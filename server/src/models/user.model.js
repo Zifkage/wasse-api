7638e399bf45a4d7b768d3fb1c9c560021c55971
Zifkage <zifkage@gmail.com>
2019-05-02 18:48:03 +0100

    config travis

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,29 +1,23 @@
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    trim: true,
-    match: [/^[\w\.+]+@\w+\.\w+$/, "The 'email' field must be a valid email"],
-    required: [true, "The 'email' field is missing"],
-  },
   password: {
     type: String,
-    required: [true, "The 'password' field is missing"],
+    required: [true, "The 'password' field is missing"]
   },
-  name: { type: String, required: [true, "The 'name' field is missing"] },
-  contact: { type: String, required: [true, "The 'contact' field is missing"] },
-  bio: { type: String, required: [true, "The 'bio' field is missing"] },
-  followers: [],
-  following: [],
+  username: { type: String, required: [true, "The 'name' field is missing"] },
+  phone_number: {
+    type: String,
+    required: [true, "The 'contact' field is missing"]
+  },
   createdAt: {
     type: Number,
-    default: Date.now,
+    default: Date.now
   },
-  updatedAt: Number,
+  updatedAt: Number
 });
 
 export default {
   model: mongoose.model('User', UserSchema),
-  schema: UserSchema,
+  schema: UserSchema
 };
